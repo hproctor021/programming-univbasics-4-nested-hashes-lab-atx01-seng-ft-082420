b311ac497e2155cbabc2832e86c8584427ec883e
Haley Proctor <hproctor021@gmail.com>
2020-08-01 19:04:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,9 +102,6 @@ return programmer_hash
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -120,5 +117,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+return programmer_hash
 end
